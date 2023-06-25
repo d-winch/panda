405adf802ee78e34b577de73075aa07cb59a989a
David Winch <31210978+d-winch@users.noreply.github.com>
2023-06-25 14:01:40 +0100

    Added basic address tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session, declarative_base, sessionmaker
 from panda import crud, models
 from panda.enums import AddressOwnerType, Sex
 from panda.models import Address, Appointment, Patient
-from panda.schemas import PatientCreate
+from panda.schemas import AddressCreate, PatientCreate
 
 Faker.seed(0)
 fake: Faker = Faker("en-GB")
@@ -81,7 +81,7 @@ def fixture_invalid_nhs_numbers() -> List[str]:
 
 @pytest.fixture(name="invalid_format_nhs_numbers")
 def fixture_invalid_format_nhs_numbers() -> List[str]:
-    """ Returns a list of badly formatted nhs numbers """
+    """ Returns a list of badly formatted but valid nhs numbers """
     return [
         ' 4609571471',
         '452 440 8592',
@@ -91,12 +91,26 @@ def fixture_invalid_format_nhs_numbers() -> List[str]:
 
 @pytest.fixture(name="valid_patient")
 def valid_patient() -> PatientCreate:
-    """ Returns a valid patient """
+    """ Returns a valid patient create object"""
     return PatientCreate(
         nhs_number="350 416 5898",
         name="David Winch",
         dob=date(1988, 12, 25),
         sex=Sex.MALE,
+    )
+
+
+@pytest.fixture(name="valid_address")
+def valid_address() -> AddressCreate:
+    """ Returns a valid address create object """
+    return AddressCreate(
+        owner_type=AddressOwnerType.PATIENT,
+        line1="69 Pendragon Crescent",
+        line2="",
+        town="Newquay",
+        county="Cornwall",
+        postcode="TR7 2SS",
+        country="UK"
     )
 
 
@@ -137,3 +151,19 @@ def test_get_patient_by_id(valid_patient: PatientCreate):
     patient_by_nhs = crud.get_patient_by_nhs_number(db=get_db(), nhs_number=valid_patient.nhs_number)
     patient_by_id = crud.get_patient_by_id(db=get_db(), patient_id=int(patient_by_nhs.id))
     assert str(patient_by_id.nhs_number) == str(patient_by_nhs.nhs_number)
+
+def test_add_patient_address(valid_address: AddressCreate, valid_patient: Patient):
+    """
+    Test adding an address to a valid patient id
+    """
+    patient_by_nhs = crud.get_patient_by_nhs_number(db=get_db(), nhs_number=str(valid_patient.nhs_number))
+    crud.create_patient_address(db=get_db(), address=valid_address, patient_id=int(patient_by_nhs.id))
+
+
+def test_add_patient_address_for_nonexistent_patient(valid_address: AddressCreate):
+    """
+    Test adding an address to an invalid patient id
+    """
+    with pytest.raises(HTTPException) as execinfo:
+        crud.create_patient_address(db=get_db(), address=valid_address, patient_id=999999999)
+    assert execinfo.value.status_code == 403
