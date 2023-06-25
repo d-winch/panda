4dbfe5cea16f4ccd49526f7e5e5208dcd04b7c90
David Winch <31210978+d-winch@users.noreply.github.com>
2023-06-25 14:14:11 +0100

    Added comment explaining regex

--- panda/schemas.py
+++ panda/schemas.py
@@ -90,6 +90,7 @@ class AddressBase(BaseModel):
     def coerce_postcode(cls, v: str) -> str:
         v = re.sub(r"[^a-zA-Z0-9]", "", v).upper()
         valid_postcode = re.compile(
+            # This is the government's postcode validation regex
             r"^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$"
         )
         match = valid_postcode.match(v)
